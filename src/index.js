18b8f2206626c9bc0edccf4bbe2f23a822d80bcc
Zach Wilson <zwilson22495@gmail.com>
2022-02-03 14:19:20 -0800

    add functionality to haiku submits
    Co-Authored-by: Matt Luker <jamesmattluker@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-// $(document).ready(function() {
-//   $('#haiku-form').submit(function() {
-//     const 
-//   })
-// }
+import { doTheDangThang, addTheDangThang } from '../src/js/haiku.js';
+
+$(document).ready(function() {
+  $('#haiku-form').submit(function() {
+    event.preventDefault();
+    console.log($('#first-line').val())
+    console.log
+    const firstLine = addTheDangThang(doTheDangThang($('#first-line').val().split(" ")));
+    console.log(firstLine);
+    const secondLine = addTheDangThang(doTheDangThang($('#second-line').val().split(" ")));
+    console.log(secondLine);
+    const thirdLine = addTheDangThang(doTheDangThang($('#third-line').val().split(" ")));
+    console.log(thirdLine);
+    if (firstLine !== 5 || secondLine !== 7 || thirdLine !== 5) {
+      $('#kuOrNo').text("This is not a haiku yo. Try again!");
+    } else {
+      $('#kuOrNo').text("What beautiful composition you have.  This is what I call a haiku.");
+    }
+  });
+});
--- src/js/haiku.js
+++ src/js/haiku.js
@@ -1,22 +1,22 @@
 
 export function exemption(string) {
   if (string.endsWith("ded")) {
-    const saved = string.replace("ded", "dod")
-    return saved
+    const saved = string.replace("ded", "dod");
+    return saved;
   } else if (string.endsWith("ted")) {
-    const saved = string.replace("ted", "tod")
-    return saved
+    const saved = string.replace("ted", "tod");
+    return saved;
   } else {
-    return string
+    return string;
   }
 }
 
 export function deleteSilent(string) {
   if (string.endsWith("e")) {
-    const trimmed = string.replace("e", "") 
+    const trimmed = string.replace("e", "");
     return trimmed;
   } else if (string.endsWith("ed")) {
-    const trimmed = string.replace("ed", "")
+    const trimmed = string.replace("ed", "");
     return trimmed;
   } else {
     return string;
@@ -41,7 +41,7 @@ export function doubleVowelRemover(string) {
     const removed = string.replace("ee", "e");
     return removed;
   } else {
-    const removed = string
+    const removed = string;
     return removed;
   }
 }
@@ -52,16 +52,6 @@ export function vowelCounter(string) {
   return counter.length;
 }
 
-// export function sentanceVowelCount(string) {
-//   const vowelCount = (string) => {
-//     const seperatedString = string.split(" ");
-//     return (arrayElementVowelCounterFunction) => {
-//       const newStringArr = arrayElementVowelCounterFunction(stringArr);
-//       stringArr = {...newStringArr};
-//     }
-//   }
-// }
-
 export function doTheDangThang (arr) {
   const syllableCount = arr.map(e => vowelCounter(doubleVowelRemover(eilizer(deleteSilent(exemption(e))))));
   return syllableCount;
@@ -70,24 +60,6 @@ export function doTheDangThang (arr) {
 export function addTheDangThang (arr) {
   const summedArray = arr.reduce(function(currentValue, element) {
     return element + currentValue;
-  }, 0)
+  }, 0);
   return summedArray;
 }
-
-// export function syllableCounter(string) {
-//   const word = string.split("");
-// }
-
-
-
-
-
-
-
-  // if (string[string.length-1] = )
-  // if (word.includes("a") || word.includes("e") || word.includes("i") || word.includes("o") || word.includes("u")) {
-  //   counter ++;
-  // } else {
-  //   return counter;
-  // }
-  // return counter; 
